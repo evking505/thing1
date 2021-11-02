bfe1effa53d2df0a8b503efc4579d9c487674bda
EVking <37128766+EVking@users.noreply.github.com>
2021-11-02 17:22:36 -0400

    The Collatz Conjecture

---
+++ thing1.py
@@ -0,0 +1,25 @@
+class Collatz:
+    #counter = 0
+
+    def __init__(self, num=-1):
+        self.num = num
+
+    def compute(self, num=-1):
+        counter = 0
+        if(num != -1):
+            self.num = num
+        if(self.num == -1):
+            print("Please use a number >1")
+            return
+        if(self.num > 1):
+            while(self.num != 1):
+                if(self.num%2 == 0):
+                    self.num /= 2
+                else:
+                    self.num = ((self.num*3)+1)
+                counter += 1
+                print(self.num)
+            return counter
+
+test1 = Collatz()
+print(test1.compute(91902348091230))
